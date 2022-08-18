2e96ffd3af408ba2788b805b29e277f83c0a041e
Heman449 <53329534+heman449@users.noreply.github.com>
2022-08-18 18:28:50 +0200

    Delete event_user.php

--- include/event_user.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$header = "<div class='p_bigger'>Eigene Events</div>";
-include("include/navigation.php");
-$user_rights = $right[$_SESSION["log_status"]];
-$ausgabe="<div class='p_mid'>Rechte: ". $user_rights ."</div>";
-?>
